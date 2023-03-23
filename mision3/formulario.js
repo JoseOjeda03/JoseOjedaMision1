bb2cce16bbdf612de71e908204417eb6817e63e8
johnmartinez <johnmartinezolarte@gmail.com>
2023-03-23 12:52:53 -0500

    arreglo parcial

--- mision3/formulario.js
+++ mision3/formulario.js
@@ -2,9 +2,11 @@
 let i=0
 const img =document.querySelector("#img")
 boton.addEventListener('click',formularioevento)
-img.addEventListener('click',formularioevento)
+img.addEventListener('click',regresar)
 
-
+ function regresar(){
+    location.href="index.html"
+ }
 function formularioevento(){
     const nombre=document.getElementById('nombre').value;
 const apellido=document.querySelector('#apellido').value;
@@ -16,11 +18,44 @@ const email=document.querySelector('#email').value;
 
 let nombreCompleto=nombre+" "+apellido;
 let nuevoUsuario =[{"nombrecompleto":nombreCompleto,"apellido":apellido,"area":area,"usuario":usuario,"edad":edad,"direccion":direccion,"email":email}]
+ if(nombreCompleto == "" || apellido == "" || usuario == "" || direccion == "" || email == ""){
+    alert("no s epueden guardar los datos si  estan nulos")
+ }
+ else{
 
-localStorage.setItem(localStorage.length, JSON.stringify(nuevoUsuario));
-
+  if(validar(usuario)){
+    alert("ususario ya digitado")
+  }
+  else{
+    localStorage.setItem(localStorage.length, JSON.stringify(nuevoUsuario));
+    
     location.href="index.html"
+  
+  }
 
+ }
+
+
+}
+function validar(usuarios){
+    let j=0;
+    let respuesta= false
+    console.log(localStorage.length)
+    for ( j = 0; j < localStorage.length; j++) {
+        var z = JSON.parse(localStorage.getItem(j));
+         console.log(z[0]["usuario"])
+        if(z[0]["usuario"]== usuarios){
+            console.log("mAMITA")
+            respuesta= true
+
+        } else{
+            respuesta= false
+           
+        }
+
+
+    }
+    return respuesta
 }
 
  
--- mision3/main.js
+++ mision3/main.js
@@ -1,8 +1,32 @@
-const boton = document.querySelector('#button')
-const docencia =document.querySelector('#docencia')
+document.addEventListener('DOMContentLoaded',()=> {
 
+    mostrar();
+	
+}); 
+
+const boton = document.querySelector('#button')
+let searching ="";
+const docencia =document.querySelector('#docencia')
+const search= document.querySelector("#search");
 const Tecnologia =document.querySelector('#Tecnologia')
 
+search.addEventListener('input',(e)=>{
+    searching =e.target.value;
+    
+    
+    if(searching !=""){
+        
+        getBuscarrr(searching);
+    } else{
+        mostrar();
+        console.log("holas")
+   
+    
+    }
+
+    
+    
+    })
 boton.addEventListener('click',ir)
 function ir(){
     location.href="formulario.html"
@@ -11,36 +35,73 @@ let contDocencia =0;
 let contTec =0;
 let contMark =0;
 
+function getBuscarrr(searching){
+    docencia.innerHTML=" "
+    for (let j = 0; j < localStorage.length; j++) {
+        var z = JSON.parse(localStorage.getItem(j));
 
+        if(z[0]["nombrecompleto"]===searching){
+            console.log("mAMITA")
+             
+            let html= ` 
+        
+               
+                <tr>
+                <td class="text-center"> ${z[0]["area"]}</td>
+                <td class="text-center"> ${z[0]["nombrecompleto"]}</td>
+                <td class="text-center"> ${z[0]["usuario"]}</td>
+                <td class="text-center text-[#C9231B]"> ${z[0]["email"]}</td>
+                <td class="text-center"> ${z[0]["edad"]}</td>
+                </tr>
+              
+             
+        
+            
+            
+            
+            `
+            docencia.innerHTML+=html
 
-
-  
-  for (let j = 0; j < localStorage.length; j++) {
-    var z = JSON.parse(localStorage.getItem(j));
-
-        console.log("hola")
-        
-        let html= ` 
-    
-           
-            <tr>
-            <td class="text-center"> ${z[0]["area"]}</td>
-            <td class="text-center"> ${z[0]["nombrecompleto"]}</td>
-            <td class="text-center"> ${z[0]["usuario"]}</td>
-            <td class="text-center text-[#C9231B]"> ${z[0]["email"]}</td>
-            <td class="text-center"> ${z[0]["edad"]}</td>
-            </tr>
-          
-         
-    
-        
-        
-        
-        `
-        docencia.innerHTML+=html
+        }
 
 
     }
+
+
+}
+
+function mostrar(){
+    for (let j = 0; j < localStorage.length; j++) {
+        var z = JSON.parse(localStorage.getItem(j));
+    
+            console.log("hola")
+            
+            let html= ` 
+        
+               
+                <tr>
+                <td class="text-center"> ${z[0]["area"]}</td>
+                <td class="text-center"> ${z[0]["nombrecompleto"]}</td>
+                <td class="text-center"> ${z[0]["usuario"]}</td>
+                <td class="text-center text-[#C9231B]"> ${z[0]["email"]}</td>
+                <td class="text-center"> ${z[0]["edad"]}</td>
+                </tr>
+              
+             
+        
+            
+            
+            
+            `
+            docencia.innerHTML+=html
+    
+    
+        }
+
+
+}
+  
+  
 
 
 
